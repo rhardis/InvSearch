0911a5cfe6aac76781d13aa49967b3923d5d6612
rhardis <richardphardis@gmail.com>
2020-10-13 06:47:39 -0400

    hmm updates
    hmm updates

--- Backtest/hmm_enddate.R
+++ Backtest/hmm_enddate.R
@@ -1,28 +1,40 @@
 remove(list = ls())
+dev.off()
+tstart = Sys.time()
 
 library('quantmod')
 library(lubridate)
 set.seed(1)
 
 getSymbols("^GSPC",from="1900-01-01")
+GSPC = to.weekly(GSPC, indexAt="startof")
 gspcRets = diff( log( Ad( GSPC ) ) )
 returns = as.numeric(gspcRets)
 returns[1] = 0
 
 gspcRets.ex = as.data.frame(cbind(gspcRets,seq(1,length(gspcRets))))
-cutoff_idx = gspcRets.ex["1960-12-22",2]
-#cutoff_idx = gspcRets.ex["2020-04-06",2]
-days_to_model = nrow(gspcRets) - cutoff_idx # Start date for backtest3 
-gpx = gspcRets.ex[(nrow(gspcRets.ex)-days_to_model+1):nrow(gspcRets.ex),]
+#cutoff_idx = gspcRets.ex["1980-06-27",2]
+cutoff_idx = gspcRets.ex["2020-06-08",2]
+periods_to_model = nrow(gspcRets) - cutoff_idx # Start date for backtest3 
+gpx = gspcRets.ex[(nrow(gspcRets.ex)-periods_to_model+1):nrow(gspcRets.ex),]
+returns = returns[(length(returns) - periods_to_model + 1):length(returns)]
+gspc.df = as.data.frame(GSPC)
+prices = gspc.df[(nrow(gspc.df)-periods_to_model+1):nrow(gspc.df),"GSPC.Adjusted"]
 
 library(doParallel)
-registerDoParallel(cores=detectCores())
+#registerDoParallel(cores=detectCores())
+library(doSNOW)
+cl = makeCluster(detectCores())
+registerDoSNOW(cl)
+pb = txtProgressBar(max = periods_to_model, style=3)
+progress = function(n) setTxtProgressBar(pb, n)
+opts = list(progress = progress)
 
-state_predictions = foreach(i = 1:days_to_model, .combine=c) %dopar% {
+state_predictions = foreach(i = 1:periods_to_model, .combine=c, .options.snow = opts) %dopar% {
   library(depmixS4)
   library(plyr)
   truncated_returns = returns[1:(cutoff_idx+i)]
-  hmm <- depmix(returns ~ 1, family = gaussian(), nstates = 4, data=data.frame(returns=truncated_returns))
+  hmm <- depmix(returns ~ 1, family = gaussian(), nstates = 2, data=data.frame(returns=truncated_returns))
   hmmfit <- fit(hmm, verbose = F)
   post_probs <- posterior(hmmfit)
   post_probs$returns = truncated_returns
@@ -34,6 +46,8 @@ state_predictions = foreach(i = 1:days_to_model, .combine=c) %dopar% {
   
   v = c(s1,s2,s3,s4)
   vnums = c(1,2,3,4)
+  # v = c(s1, s2)
+  # vnums = c(1,2)
   vdf = data.frame(cbind(v,vnums))
   vdf = vdf[order(vdf$v),]
   
@@ -44,23 +58,55 @@ state_predictions = foreach(i = 1:days_to_model, .combine=c) %dopar% {
   
   cstate = post_probs$state[nrow(post_probs)]
   
-  #write.csv(sprintf("postprobs_%i", i))
-  
-  if (cstate==correction){
-   pred = "Level4"
+  if (cstate==lvol){
+   pred = "Level1"
   }
   else if (cstate==mvol){
     pred = "Level2"
   }
-  else if (cstate==lvol){
-    pred = "Level1"
+  else if (cstate==correction){
+    pred = "Level4"
   }
   else{
     pred = "Level3"
   }
-  to.state_predictions = pred
-  to.state_predictions
+  # to.state_predictions = pred
+  # to.state_predictions
+  return(post_probs)
 }
+close(pb)
+stopCluster(cl)
 
-preds_df = cbind(gpx,state_predictions)
-write.csv(preds_df, sprintf("C:\\Users\\richa\\Documents\\GitHub\\InvSearch\\data\\state_predictions_1960.csv", days_to_model))
+preds_df = cbind(gpx,state_predictions,returns,prices)
+names(preds_df)[names(preds_df) == "prices"] = "Close.Price"
+names(preds_df)[names(preds_df) == "state_predictions"] = "State_Name"
+preds_df["date"] = ymd(rownames(as.data.frame(gspcRets[(nrow(gspcRets)-periods_to_model+1):nrow(gspcRets),])))
+write.csv(preds_df, sprintf("C:\\Users\\richa\\Documents\\GitHub\\InvSearch\\data\\state_predictions_weekly_test.csv", periods_to_model))
+
+library(patchwork)
+library(ggplot2)
+
+densall = ggplot(data=preds_df, aes(x=returns))+
+  geom_density(aes(color=State_Name))+
+  ggtitle("Density of Log\nReturns by Regime")+xlim(-.1,.1)
+
+close.p =  ggplot(preds_df, aes(x=date, y=Close.Price, color=State_Name)) +
+  geom_line()+
+  ylab("Closing Price ($)")+
+  scale_x_date(date_labels = "%m-%Y")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  ggtitle("Closing Prices")
+
+p = ggplot(preds_df, aes(x=date, y=returns, color=State_Name)) +
+  geom_point()+
+  ylab("log returns")+
+  scale_x_date(date_labels = "%m-%Y")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  ggtitle("Regime Predictions")
+
+densall + (p / close.p)
+
+
+tend = Sys.time()
+print(tend-tstart)
+
---
+++ Backtest/hmm_plots (1).R
@@ -0,0 +1,193 @@
+remove(list = ls())
+
+# Obtain S&P500 data from 2004 onwards and
+# create the returns stream from this
+
+library('depmixS4')
+library('quantmod')
+library('gridExtra')
+library('patchwork')
+library(ggplot2)
+library(lubridate)
+library(plyr)
+set.seed(1)
+getSymbols( "^GSPC", from="1927-12-30")
+GSPC = to.weekly(GSPC, indexAt="startof")
+gspcRets = diff( log( Cl( GSPC ) ) )
+returns = as.numeric(gspcRets)
+returns[1] = 0
+plot(gspcRets)
+
+ggplot(gspcRets, aes(GSPC.Close))+ geom_density()+
+  ggtitle("Distribution of Daily Log Returns\n1927-Present") +
+  xlab("Return") + ylab("Density")+
+  xlim(-.1,.1)
+
+mean(returns)
+
+density(returns)
+
+
+# Fit a Hidden Markov Model with three states 
+# to the S&P500 returns stream
+hmm <- depmix(returns ~ 1, family = gaussian(), nstates = 3, data=data.frame(returns=returns))
+hmmfit <- fit(hmm, verbose = FALSE)
+post_probs <- posterior(hmmfit)
+
+post_probs$returns = returns
+
+s1 = sd(post_probs[post_probs$state==1,]$returns)
+s2 = sd(post_probs[post_probs$state==2,]$returns)
+s3 = sd(post_probs[post_probs$state==3,]$returns)
+#s4 = sd(post_probs[post_probs$state==4,]$returns)
+s1
+s2
+s3
+# s4
+
+v = c(s1,s2,s3)
+vnums = c(1,2,3)
+vdf = data.frame(cbind(v,vnums))
+vdf = vdf[order(vdf$v),]
+
+lvol = vdf$vnums[1]
+mvol = vdf$vnums[2]
+hvol = vdf$vnums[3]
+
+label = function(row){
+  cstate = row[1]
+  
+  if (cstate==mvol){
+    pred = "MedVol"
+  }
+  else if (cstate==lvol){
+    pred = "LowVol"
+  }
+  else{
+    pred = "HighVol"
+  }
+  return(pred)
+}
+
+post_probs["State_Name"] = apply(post_probs,1,label)
+
+# Plot the returns stream and the posterior
+# probabilities of the separate regimes
+layout(1:2)
+plot(returns, type='l', main='Regime Detection', xlab='', ylab='Returns')
+abline(0,0, col="red")
+matplot(post_probs[,-1], type='l', main='Regime Posterior Probabilities', ylab='Probability')
+legend(x='bottomleft', c('Regime #1','Regime #2', 'Regime #3', 'Regime #4'), fill=1:3, bty='n')
+
+gspcRets = as.data.frame(gspcRets)
+post_probs$state = as.factor(post_probs$state)
+post_probs$date = ymd(rownames(gspcRets))
+post_probs["Close.Price"] = GSPC$GSPC.Close
+
+dev.off()
+
+densall = ggplot(data=post_probs, aes(x=returns))+
+  geom_density(aes(color=State_Name))+
+  ggtitle("Density of Log\nReturns by Regime")+xlim(-.1,.1)
+
+close.p =  ggplot(post_probs[post_probs$date >= "2019-11-01",], aes(x=date, y=Close.Price, color=State_Name)) +
+  geom_line()+
+  ylab("Closing Price ($)")+
+  scale_x_date(date_labels = "%m-%Y")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  ggtitle("Closing Prices\nNovember 2019 - April 2020")
+
+p <- ggplot(post_probs[post_probs$date >= "2019-11-01",], aes(x=date, y=returns, color=State_Name)) +
+  geom_point()+
+  ylab("log returns")+
+  scale_x_date(date_labels = "%m-%Y")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  ggtitle("Regime Predictions\nNovember 2019 - April 2020")
+
+#grid.arrange(densall, p, ncol=2)
+
+densall + (p / close.p)
+
+# The color labels below may be inaccurate.  The backtesting R script dynamically lables them correctly
+p2 <- ggplot(post_probs[,], aes(x=date)) +
+  geom_line(aes(y=S1, colour="Transition"), size=1.2)+
+  geom_line(aes(y=S2, colour="LowVol"), size=1.2)+
+  geom_line(aes(y=S3, colour="HighVol"), size=1.2)+
+  ylab("Probability of State")+
+  scale_x_date(date_labels = "%d-%m-%Y")
+p2
+
+p3 <- ggplot(post_probs, aes(x=date)) +
+  geom_line(aes(y=S1, colour="s1"))+
+  geom_line(aes(y=S2, colour="s2"))+
+  geom_line(aes(y=S3, colour="s3"))+
+  ylab("Probability of State")+
+  scale_x_date(date_labels = "%d-%m-%Y")
+p3
+
+highvol.periods = c()
+medvol.periods = c()
+lowvol.periods = c()
+previous_state = ""
+count = 1
+for (i in 1:nrow(post_probs)){
+  current_state = post_probs$State_Name[i]
+  
+  if (current_state == previous_state){
+    count = count+1
+  }
+  else if (current_state != previous_state){
+    print(post_probs$date[i])
+    if(previous_state == "HighVol"){
+      highvol.periods = c(highvol.periods, count)
+    }
+    else if (previous_state == "MedVol"){
+      medvol.periods = c(medvol.periods, count)
+    }
+    else if (previous_state == "LowVol"){
+      lowvol.periods = c(lowvol.periods, count)
+    }
+    count = 1
+  }
+  if ((i == nrow(post_probs)) & (count > 1)){
+    if(current_state == "HighVol"){
+      highvol.periods = c(highvol.periods, count)
+    }
+    else if (current_state == "MedVol"){
+      medvol.periods = c(medvol.periods, count)
+    }
+    else if (current_state == "LowVol"){
+      lowvol.periods = c(lowvol.periods, count)
+    }
+  }
+  previous_state = current_state
+}
+
+hvdf = as.data.frame(highvol.periods)
+mvdf = as.data.frame(medvol.periods)
+lvdf = as.data.frame(lowvol.periods)
+
+hvpeak = density(hvdf$highvol.periods)$x[which.max(density(hvdf$highvol.periods)$y)]
+mvpeak = density(mvdf$medvol.periods)$x[which.max(density(mvdf$medvol.periods)$y)]
+lvpeak = density(lvdf$lowvol.periods)$x[which.max(density(lvdf$lowvol.periods)$y)]
+
+hvstats = c(mean(highvol.periods),min(highvol.periods), max(highvol.periods), hvpeak,length(highvol.periods))
+mvstats = c(mean(medvol.periods),min(medvol.periods), max(medvol.periods),mvpeak,length(medvol.periods))
+lvstats = c(mean(lowvol.periods),min(lowvol.periods), max(lowvol.periods),lvpeak,length(lowvol.periods))
+regime_stats = as.data.frame(rbind(lvstats, mvstats, hvstats), row.names = c("Low Volatility","Medium Volatility","High Volatility"))
+colnames(regime_stats) = c("Mean Length","Minimum Length","Maximum Length","Peak of Density Length","Number of Periods")
+
+densh = ggplot(hvdf, aes(highvol.periods))+ geom_density()+ geom_vline(xintercept = hvpeak)+ggtitle("Length of High\nVolatility Periods") + xlab("Days") + ylab("Density")
+densm = ggplot(mvdf, aes(medvol.periods))+ geom_density()+ geom_vline(xintercept = mvpeak)+ggtitle("Length of Medium\nVolatility Periods") + xlab("Days") + ylab("Density")
+densl = ggplot(lvdf, aes(lowvol.periods))+ geom_density()+ geom_vline(xintercept = lvpeak)+ggtitle("Length of Low\nVolatility Periods") + xlab("Days") + ylab("Density")
+grid.arrange(densl, densm, densh, ncol=3)
+
+volsize = 0.033
+
+num = nrow(post_probs[abs(post_probs$returns) >= volsize,])
+
+cor = nrow(post_probs[(abs(post_probs$returns) >= volsize) & (post_probs$State_Name == "HighVol"),])
+
+cor
+num
+cor/num
